b19e9fcf8f3777d22124ea46ab88814100ebdbb0
Rod770 <vr579269@gmail.com>
2024-10-29 12:19:32 -0400

    correccion 0.0

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rod770.py
@@ -0,0 +1,18 @@
+#https://www.python.org/
+
+#una linea de para comentario
+
+""" multiples linea para el comentario"""
+
+coci_Nando = "restaurante"
+
+CONSTANTE = "no la cambies" # esto es por convencion
+
+mi_int = 2
+mi_float = 2.5
+mi_bool = True
+mi_bool = False
+mi_string = "cualquiera"
+other_string = 'simples'
+
+print("Hola, Python")
